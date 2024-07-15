af2780e036661e4d5bdd89d4ac794c4c81c7479e
mami <imua.13137@gmail.comm>
2024-07-15 09:20:32 +0900

    スライダーの設定変更

--- js/script.js
+++ js/script.js
@@ -77,14 +77,23 @@ $(window).on('load', function(){
 	});
 });
 
+// case スライダー
+
 const mySwiper = new Swiper('.swiper-container', {
   // オプション設定
   loop: true, // ループするかどうか
 	loopAdditionalSlides: 1, // 無限ループさせる場合に複製するスライド数
+	
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-	slidesPerView: 2, // 3つのスライドを表示
+	slidesPerView: 1,
+  breakpoints: {
+    // スライドの表示枚数：500px以上の場合
+    500: {
+      slidesPerView: 2,
+    }
+  },
 	spaceBetween: 0, // スライド間の余白（px）
 });
